a1a7a74db1f367075ef335570b966d30171ac89d
Linh Trinh <103659319+linhtr59@users.noreply.github.com>
2022-10-20 14:29:43 +0800

    Update trove.h

--- trove.h
+++ trove.h
@@ -12,11 +12,10 @@
 
 
 // PREPROCESSOR CONSTANTS
-#define	OPTION_LIST "bruf:l:"
-#define DEFAULT_VALUE 4
-#define DEFAULT_NAME "/tmp/trove"
+#define	OPTION_LIST "brul::f"
 #define _POSIX_C_SOURCE     200809L
 #define	CHECK_ALLOC(p) if(p == NULL) { perror(__func__); exit(EXIT_FAILURE); }
+#define HASHTABLE_SIZE 10000
 
 
 //our hashtable will essentially be like an array of filename: word key value pairds
@@ -38,10 +37,22 @@ typedef INDEXFILE * HASHTABLE;
 
 // GLOBAL FUNCTIONS
 
-extern void build(int argc, char *argv[], HASHTABLE hashtable, int word_length);
-extern void scan_directory (char *dirname);
-extern bool is_valid_word(char *word, int value);
+//BUILDING FUNCTIONS 
+extern HASHTABLE *trove_to_hashtable(void);
+extern void change_name(char *filename);
+extern char *wordlist_read(LIST *wordlist);
+extern char *hashtable_read(HASHTABLE *hashtable);
+extern void build(int argc, char *argv[]);
+extern void trove_write(HASHTABLE *hashtable);
 
+
+//FILE PROCESSING FUNCTIONS TO CONSTRUCT HASHTABLES
+extern void scan_directory (char *dirname, HASHTABLE *hashtable, LIST *wordlist);
+extern void file_reader(char *filename, HASHTABLE *hashtable, LIST *wordlist);
+
+
+
+//LIST FUNCTIONS
 extern LIST *list_new(void);
 extern bool list_find(LIST *list, char *wanted);
 extern LIST *list_new_item(char *word);
@@ -51,12 +62,21 @@ extern void list_print(LIST *list);
 
 
 
-
-extern HASHTABLE hashtable_new();
+//HASHTABLE FUNCTIONS
+extern HASHTABLE *hashtable_new(void);
 extern void hashtable_add(HASHTABLE *hashtable, char *filename, LIST *word);
-extern INDEXFILE *hashtable_search(HASHTABLE *hashtable, char *filename);
+extern void hashtable_remove(HASHTABLE *hashtable, char *target);
+extern LIST *hashtable_findword(HASHTABLE *hashtable, char *target);
+extern void hashtable_update(HASHTABLE *hashtable1, HASHTABLE *hashtable2);
 extern void hashtable_print(HASHTABLE *hashtable);
+
+
+//SEARCH WORD FUNCTIONS
+extern void find_word(int argc, char *argv[]);
+
 
 //GLOBAL VARIABLES
 extern int word_length;
-extern HASHTABLE hashtable;
+extern char *trovefile;
+extern char *filelist;
+
