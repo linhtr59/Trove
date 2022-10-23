d29e2fb994cf068c0e28c81c107f971f673395d2
Linh Trinh <103659319+linhtr59@users.noreply.github.com>
2022-10-23 19:10:03 +0800

    Add files via upload

---
+++ globals.c
@@ -0,0 +1,7 @@
+#include "trove.h"
+//DEFINE GLOBAL VARIABLES REQUIRED BY ALL FILES 
+
+
+int word_length = 4; // intialized word length to be default value of 4
+char *trovefile = "/tmp/trove"; //intialized trovefile name to be default name /tmp/trove
+char *filelist = NULL;
