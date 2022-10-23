4a61306341c0681cf323dc150ba51d914e785f09
Linh Trinh <103659319+linhtr59@users.noreply.github.com>
2022-10-23 19:14:26 +0800

    Update update.c

--- update.c
+++ update.c
@@ -3,7 +3,15 @@
 #include "trove.h"
 
 void update(char *trovefile, int word_length, char *filelist){
-
+  HASHTABLE *update_hashtable = hashtable_new(); //new hashtable to update values with
+  LIST *update_list = list_new(); // new list to update values with
+  scan_directory(filelist, update_hashtable, update_list); //construct the hashtable to update the values with 
+  
+  //open trovefile
+  //convert trovefile to hashtable
+  //update the values of trove hashtbale to that of update_hashtable if filename mathces
+  //write trove file again and compress
+ 
 }
 
 
