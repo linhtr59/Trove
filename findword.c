05726d510780a1ac13fd52e973730b54dc9eb7f6
Linh Trinh <103659319+linhtr59@users.noreply.github.com>
2022-10-23 19:19:42 +0800

    Update findword.c

--- findword.c
+++ findword.c
@@ -4,6 +4,10 @@
 
 void find_word(char *word, char *trovefile){
     
+    //open trovefile
+    //convert trovefile to hashtable
+    //iterate search hashtbale and if find word, add filename to a list and retrun that list
+    
     
 }
 
